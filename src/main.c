ffb709e47ab8cfd2922daee9f90a7e34d60c6f79
hossein sharifi <sharifi1102000@gmail.com>
2024-11-08 00:09:50 +0330

    Add initialize function for spi.

--- src/main.c
+++ src/main.c
@@ -16,28 +16,20 @@
 #define BUFFER_SIZE			10
 
 /************************** Variable Definitions *****************************/
-XSpi  SpiInstance;	 	/* The instance of the SPI device */
-u8 	  WriteBuffer[BUFFER_SIZE];
+XSpi SpiInstance;	 	/* The instance of the SPI device */
+XSpi_Config *ConfigPtr;	/* Pointer to Configuration data */
 
+u8 WriteBuffer[BUFFER_SIZE];
+
+/************************** Function Definitions *****************************/
+void SPI_Initialize(XSpi *InstancePtr, u16 DeviceId);
+
+/****************************** Main Function ********************************/
 int main(void)
 {
-	XSpi_Config *ConfigPtr;	/* Pointer to Configuration data */
 
-	// Initialize the SPI driver so that it is  ready to use.
-	ConfigPtr = XSpi_LookupConfig(SPI_DEVICE_ID);
-	XSpi_CfgInitialize(&SpiInstance, ConfigPtr, ConfigPtr->BaseAddress);
+	SPI_Initialize(&SpiInstance, SPI_DEVICE_ID);
 
-	// Set the Spi device as a master
-	XSpi_SetOptions(&SpiInstance, XSP_MASTER_OPTION| XSP_MANUAL_SSELECT_OPTION );
-
-	// Start the SPI driver so that the device is enabled.
-	XSpi_Start(&SpiInstance);
-
-	//select slave number which wanted to transmit data//
-	XSpi_SetSlaveSelect(&SpiInstance, 1);
-
-	// Disable Global interrupt to use polled mode operation
-	XSpi_IntrGlobalDisable(&SpiInstance);
 
 	// Initialize the write buffer with pattern to write
 	for (u8 i = 0; i < BUFFER_SIZE; i++)
@@ -67,7 +59,26 @@ int main(void)
 		while (!(Xil_In32(SpiInstance.BaseAddr + XSP_SR_OFFSET) & XSP_SR_TX_EMPTY_MASK));
 	}
 
-
-
 	XSpi_WriteReg(SpiInstance.BaseAddr, XSP_SSR_OFFSET, 0xf);
+}
+
+void SPI_Initialize(XSpi *InstancePtr, u16 DeviceId)
+{
+	// Initialize the SPI driver so that it is  ready to use.
+//	ConfigPtr = XSpi_LookupConfig(SPI_DEVICE_ID);
+//	XSpi_CfgInitialize(&SpiInstance, ConfigPtr, ConfigPtr->BaseAddress);
+	XSpi_Initialize(InstancePtr, DeviceId);
+
+
+	// Set the Spi device as a master
+	XSpi_SetOptions(&SpiInstance, XSP_MASTER_OPTION| XSP_MANUAL_SSELECT_OPTION );
+
+	// Start the SPI driver so that the device is enabled.
+	XSpi_Start(&SpiInstance);
+
+	//select slave number which wanted to transmit data//
+	XSpi_SetSlaveSelect(&SpiInstance, 1);
+
+	// Disable Global interrupt to use polled mode operation
+	XSpi_IntrGlobalDisable(&SpiInstance);
 }
